35638c9c1e1745ef6cc98d3a783528269ec340cd
Bailejd <bailejd@bgsu.edu>
2021-01-21 18:13:42 -0500

    Add code to calculate and display item drop levels

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
-const MAX_LIGHT = 1260;
-const SOFT_CAP = 1250;
+const MAX_LIGHT = 1260;     // Maximum Base Power Level this season
+const SOFT_CAP = 1050;      // Technically this is a thing (1050 to 1200), could be the default for the page display
+const HARD_CAP = 1200;      // "Hard Cap" starts here (1200 to 1250)
+const PINNACLE_CAP = 1250;  // "Pinnacle Cap" starts here (1250 to 1260)
 
 let equipmentLevels = [];
 let equipmentObjDict = {};
@@ -23,6 +25,15 @@ const summary = document.getElementById("summary");
 
 const lightInputs = document.getElementsByClassName("light-input");
 const artifactInput = document.getElementById("artifact-light");
+
+const dropBPL = document.getElementById("dropBPL");
+const blueDropLevel = document.getElementById("blueDropLevel");
+const legdDropLevel = document.getElementById("legdDropLevel");
+const pow1DropLevel = document.getElementById("pow1DropLevel");
+const pow2DropLevel = document.getElementById("pow2DropLevel");
+const pow3DropLevel = document.getElementById("pow3DropLevel");
+const pin1DropLevel = document.getElementById("pin1DropLevel");
+const pin2DropLevel = document.getElementById("pin2DropLevel");
 
 function calcLevel() {
     let itemLevels = [];
@@ -55,4 +66,101 @@ function calcLevel() {
     <p><strong>With Artifact: ${Math.trunc(avgLevel + parseFloat(artifactInput.value))}</strong></p>
     `;
 
+    calcDrops(Math.trunc(avgLevel));
+}
+
+function calcDrops(basePowerLevel) {
+    // Calculate the power level of each type of drop at current Base Power Level
+    let drops = [0, 0, 0, 0, 0, 0, 0];
+
+    // Blue
+    // I am not quite sure how blue drops work before 1200
+    // For now it will always be BPL + 0
+    drops[0] = basePowerLevel;
+
+    // Legendary
+    // I am not quite sure how legendary drops work before 1200
+    // For now it will always be BPL + 0
+    drops[1] = basePowerLevel;
+
+    // Powerful Tier 1
+    if (basePowerLevel < PINNACLE_CAP) {
+        // I am not sure if drops while BPL is under a cap can drop over the cap since drop values change
+        // For now drops over PINNACLE_CAP while BPL is under PINNACLE_CAP drop at PINNACLE_CAP
+        if (basePowerLevel + 3 > PINNACLE_CAP) {
+            drops[2] = PINNACLE_CAP;
+        }
+        else {
+            drops[2] = basePowerLevel + 3;
+        }
+    }
+    else {
+        drops[2] = basePowerLevel;
+    }
+
+    // Powerful Tier 2
+    if (basePowerLevel < PINNACLE_CAP) {
+        // I am not sure if drops while BPL is under a cap can drop over the cap since drop values change
+        // For now drops over PINNACLE_CAP while BPL is under PINNACLE_CAP drop at PINNACLE_CAP
+        if (basePowerLevel + 4 > PINNACLE_CAP) {
+            drops[3] = PINNACLE_CAP;
+        }
+        else {
+            drops[3] = basePowerLevel + 4;
+        }
+    }
+    else {
+        drops[3] = basePowerLevel;
+    }
+
+    // Powerful Tier 3
+    if (basePowerLevel < PINNACLE_CAP) {
+        // I am not sure if drops while BPL is under a cap can drop over the cap since drop values change
+        // For now drops over PINNACLE_CAP while BPL is under PINNACLE_CAP drop at PINNACLE_CAP
+        if (basePowerLevel + 5 > PINNACLE_CAP) {
+            drops[4] = PINNACLE_CAP;
+        }
+        else {
+            drops[4] = basePowerLevel + 5;
+        }
+    }
+    else {
+        drops[4] = basePowerLevel;
+    }
+
+    // Pinnacle Weak
+    if (basePowerLevel < PINNACLE_CAP) {
+        if (basePowerLevel + 4 > PINNACLE_CAP) {
+            drops[5] = PINNACLE_CAP;
+        }
+        else {
+            drops[5] = basePowerLevel + 4;
+        }
+    }
+    else {
+        drops[5] = basePowerLevel + 1;
+    }
+
+    // Pinnacle Strong
+    if (basePowerLevel < PINNACLE_CAP) {
+        if (basePowerLevel + 5 > PINNACLE_CAP) {
+            drops[6] = PINNACLE_CAP;
+        }
+        else {
+            drops[6] = basePowerLevel + 5;
+        }
+    }
+    else {
+        drops[6] = basePowerLevel + 2;
+    }
+
+    // Update drop level table
+    dropBPL.innerHTML = `Drop Levels at Base Power Level: ${basePowerLevel}`;
+    blueDropLevel.innerHTML = drops[0];
+    legdDropLevel.innerHTML = drops[1];
+    pow1DropLevel.innerHTML = drops[2];
+    pow2DropLevel.innerHTML = drops[3];
+    pow3DropLevel.innerHTML = drops[4];
+    pin1DropLevel.innerHTML = drops[5];
+    pin2DropLevel.innerHTML = drops[6];
 }
